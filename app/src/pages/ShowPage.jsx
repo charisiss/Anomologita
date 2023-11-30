ac0eb9915d4578855708c64c95e1ee0c8b5e5939
Theodor <71594408+AmpasTheodoros@users.noreply.github.com>
2023-11-30 16:52:15 +0200

    Like Button

--- app/src/pages/ShowPage.jsx
+++ app/src/pages/ShowPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { collection, query, where, onSnapshot, orderBy } from "firebase/firestore";
 import { db } from "../services/firebaseConfig.js"; // Import your Firebase config
 import Wrapper from "../components/Layout/Wrapper";
 import MessageComponent from "../components/MessageComponent";
@@ -9,10 +9,13 @@ export default function ShowPage() {
 
   useEffect(() => {
     // Set up a real-time listener using onSnapshot
+    // and order the results by the ticket number in descending order
     const q = query(
       collection(db, "anomologita"),
-      where("completed", "==", true)
+      where("completed", "==", true),
+      orderBy("ticket", "desc")
     );
+  
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const items = querySnapshot.docs.map((doc) => ({
         id: doc.id,
@@ -20,10 +23,20 @@ export default function ShowPage() {
       }));
       setCompletedItems(items);
     });
-
-    // Detach the listener when the component is unmounted
-    return unsubscribe;
+  
+    return unsubscribe; // Detach the listener when the component is unmounted  
   }, []);
+
+  const handleLike = (docId) => {
+    // Assuming 'docId' is the ID of the document in Firestore
+    const docRef = doc(db, "anomologita", docId);
+    updateDoc(docRef, {
+      likes: increment(1) // Using Firestore's increment to update like count
+    }).catch(error => {
+      console.error("Error updating likes: ", error);
+    });
+  };
+
   return (
     <Wrapper hideHeaderMenu="true">
       <div className="flex flex-col items-center w-full h-[87vh] gap-10 relative">
@@ -57,10 +70,9 @@ export default function ShowPage() {
             />
           </div>
         </div>
-        <div className="grid grid-cols-3 grid-rows-3 gap-10 h-4/5 p-10">
-          {completedItems.map((item) => (
-            // eslint-disable-next-line react/jsx-key
-            <MessageComponent title={item.field1} likeCount={item.likes} />
+          <div className="grid grid-cols-3 grid-rows-3 gap-10 h-4/5 p-10">
+          {completedItems.map((item, index) => (
+            <MessageComponent key={item.id} title={item.field1} likeCount={0} onLike={() => handleLike(someDocId)} />
           ))}
         </div>
       </div>
