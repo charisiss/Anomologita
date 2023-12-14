253274ad02706ab7ec5f9d4ff6297020064e2a04
ch.samaras <54262610+ch.samaras@users.noreply.github.com>
2023-12-14 19:09:01 +0200

    Manos Changes

--- app/src/pages/ShowPage/ShowPage.jsx
+++ app/src/pages/ShowPage/ShowPage.jsx
@@ -49,14 +49,8 @@ export default function ShowPage() {
 
   return (
     <Wrapper hideHeaderMenu="true" fullWidth={"true"}>
-      <div className="relative flex w-full flex-col items-center gap-10">
-        <div className="absolute bottom-0 right-0 z-20">
-          <p className="text-xl">SCAN TO WRITE</p>
-
-          <img className="w-44 rounded-tl-xl" src="/qr-red.png"></img>
-        </div>
-
-        <div className="w-full p-10">
+      <div className="gap-15 relative flex w-full flex-col items-center">
+        <div className="w-full">
           <h2 className="pb-3 text-center text-5xl font-bold text-white ">
             TOP 3
           </h2>
@@ -68,29 +62,8 @@ export default function ShowPage() {
                 likeCount={item.likes}
                 onLike={() => handleLike(item.id)}
               />
-
-              // <MessageComponent
-              //   key={item.id}
-              //   title={item.field1}
-              //   likeCount={item.likes}
-              //   color={"red"}
-              //   stroke={"white"}
-              //   onLike={() => handleLike(item.id)}
-              //   customClass={index != 1 ? "" : "drop-shadow-2xl"}
-              // />
             ))}
           </div>
-        </div>
-        <div className="grid h-4/5 w-4/5 grid-cols-3 grid-rows-3 gap-10 p-10">
-          {completedItems.slice(-9).map((item) => (
-            // Yours
-            <ShowCard
-              key={`${item.id}-${item.likes}`} // Change the key when likes change
-              message={item.field1}
-              likeCount={item.likes}
-              onLike={() => handleLike(item.id)}
-            />
-          ))}
         </div>
 
         <div className="col-span-12 grid grid-cols-12 grid-rows-1 gap-5 pt-16">
